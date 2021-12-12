3c53a27a9e072366a31267cb39ed5ba0dc7e737c
RAM111111 <r.r.99@windowslive.com>
2021-12-12 11:35:25 +0300

    Initial Commit

---
+++ MadLibs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MadLibs
+//
+//  Created by R on 08/05/1443 AH.
+//  Copyright © 1443 R. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
